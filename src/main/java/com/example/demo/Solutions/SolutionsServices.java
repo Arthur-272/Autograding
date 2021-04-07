124ffc7821e96c28e2a0cc55bdef33a990d19b06
Radhey Patel <46446130+Arthur-272@users.noreply.github.com>
2021-04-07 20:19:04 +0530

    Removed the dependency of Windows while evaluating.

--- src/main/java/com/example/demo/Solutions/SolutionsServices.java
+++ src/main/java/com/example/demo/Solutions/SolutionsServices.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.demo.Others.Constants.OS;
+import static com.example.demo.Others.Constants.SUBMISSIONS_FOLDER;
 import static org.junit.Assert.assertEquals;
 
 @Service
@@ -58,27 +60,25 @@ public class SolutionsServices {
                 if (testCases == null)
                     return ResponseEntity.noContent().build();
 
-                String currentProjectDirectory = getCurrentProjectDirectory();
-                currentProjectDirectory += "\\src\\main\\resources\\Submissions\\";
-                File userSolution = new File(currentProjectDirectory + file.getOriginalFilename());
+                File userSolution = new File(SUBMISSIONS_FOLDER + file.getOriginalFilename());
                 file.transferTo(userSolution);
                 int totalTestCases = problem.get().getNumOfTestCases();
                 int testCasesPassed = 0;
                 switch (language) {
                     case "python3":
-                        testCasesPassed = evaluatePython(testCases, userSolution, currentProjectDirectory, 3);
+                        testCasesPassed = evaluatePython(testCases, userSolution, 3);
                         break;
                     case "python2":
-                        testCasesPassed = evaluatePython(testCases, userSolution, currentProjectDirectory, 2);
+                        testCasesPassed = evaluatePython(testCases, userSolution, 2);
                         break;
                     case "java":
-                        testCasesPassed = evaluateJava(testCases, userSolution, currentProjectDirectory);
+                        testCasesPassed = evaluateJava(testCases, userSolution);
                         break;
                     case "C":
-                        testCasesPassed = evaluateC(testCases, userSolution, currentProjectDirectory);
+                        testCasesPassed = evaluateC(testCases, userSolution);
                         break;
                     case "CPP":
-                        testCasesPassed = evaluateCPP(testCases, userSolution, currentProjectDirectory);
+                        testCasesPassed = evaluateCPP(testCases, userSolution);
                         break;
 
                 }
@@ -119,11 +119,11 @@ public class SolutionsServices {
         }
     }
 
-    public int compileAndRun(String cmd, String output) throws Exception{
+    public int compileAndRun(String[] cmd, String output) throws Exception{
         Process process;
         int count = 0;
         try {
-            process = Runtime.getRuntime().exec(cmd);
+            process = new ProcessBuilder(cmd).start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String userOutput = "";
             String line;
@@ -149,37 +149,71 @@ public class SolutionsServices {
         return count;
     }
 
-    private int evaluateC(List<TestCases> testCases, File file, String currentProjectDirectory) throws Exception{
+    private int evaluateC(List<TestCases> testCases, File file) throws Exception{
         int count = 0;
-        String cmd = "cmd /c cd " + currentProjectDirectory + " && gcc " + file.getName();
+        String[] cmd = new String[0];
+
+        if(OS.contains("windows"))
+            cmd = new String[]{
+                    "cmd",
+                    "/c",
+                    "cd " + SUBMISSIONS_FOLDER + " && gcc " + file.getName()
+            };
+
+        else if(OS.contains("linux") | OS.contains("mac"))
+            cmd = new String[]{
+                    "/bin/bash",
+                    "-c",
+                    "cd " + SUBMISSIONS_FOLDER + " && gcc " + file.getName()
+            };
+
         Process process = Runtime.getRuntime().exec(cmd);
         process.waitFor();
         for (TestCases testCase : testCases) {
             String input = testCase.getInput();
             String output = testCase.getOutput();
 
-            cmd = "cmd /c cd " + currentProjectDirectory;
-            if(System.getProperty("os.name").toLowerCase().contains("windows"))
-                cmd += " && a.exe " + input;
-            else if(System.getProperty("os.name").toLowerCase().contains("linux"))
-                cmd += " && ./a.out " + input;
+            if(OS.contains("windows"))
+                cmd = new String[]{
+                        "cmd",
+                        "/c",
+                        "cd " + SUBMISSIONS_FOLDER + " && a.exe " + input
+                };
+            else if(OS.contains("linux") | OS.contains("mac"))
+                cmd = new String[]{
+                        "/bin/bash",
+                        "-c",
+                        "cd " + SUBMISSIONS_FOLDER + " && ./a.out " + input
+                };
             count += compileAndRun(cmd, output);
         }
 
         file.delete();
-        if(System.getProperty("os.name").toLowerCase().contains("windows"))
-            file = new File(currentProjectDirectory + "a.exe");
-        else if(System.getProperty("os.name").toLowerCase().contains("linux"))
-            file = new File(currentProjectDirectory + "a.out");
+        if(OS.contains("windows"))
+            file = new File(SUBMISSIONS_FOLDER + "a.exe");
+        else if(OS.contains("linux") | OS.contains("mac"))
+            file = new File(SUBMISSIONS_FOLDER + "a.out");
         file.delete();
 
         return count;
     }
 
-    private int evaluateCPP(List<TestCases> testCases, File file, String currentProjectDirectory) throws Exception{
+    private int evaluateCPP(List<TestCases> testCases, File file) throws Exception{
         System.out.println("In C++");
         int count = 0;
-        String cmd = "cmd /c cd " + currentProjectDirectory + " && g++ -std=c++11 " + file.getName();
+        String[] cmd = new String[0];
+        if(OS.contains("windows"))
+            cmd = new String[]{
+                    "cmd",
+                    "/c",
+                    "cd " + SUBMISSIONS_FOLDER + " && g++ -std=c++11 " + file.getName()
+            };
+        else if(OS.contains("linux") | OS.contains("mac"))
+            cmd = new String[]{
+                    "/bin/bash",
+                    "-c",
+                    "cd " + SUBMISSIONS_FOLDER + " && g++ -std=c++11 " + file.getName()
+            };
         System.out.println(cmd);
         Process process = Runtime.getRuntime().exec(cmd);
         process.waitFor();
@@ -187,64 +221,108 @@ public class SolutionsServices {
             String input = testCase.getInput();
             String output = testCase.getOutput();
 
-            cmd = "cmd /c cd " + currentProjectDirectory;
-            if(System.getProperty("os.name").toLowerCase().contains("windows"))
-                cmd += " && a.exe " + input;
-            else if(System.getProperty("os.name").toLowerCase().contains("linux"))
-                cmd += " && ./a.out " + input;
+            if(OS.contains("windows"))
+                cmd = new String[]{
+                        "cmd",
+                        "/c",
+                        "cd " + SUBMISSIONS_FOLDER + " && a.exe " + input
+                };
+
+            else if(OS.contains("linux") | OS.contains("mac"))
+                cmd = new String[]{
+                        "cmd",
+                        "/c",
+                        "cd " + SUBMISSIONS_FOLDER + " && ./a.out " + input
+                };
+
             System.out.println(cmd);
             count += compileAndRun(cmd, output);
         }
 
         file.delete();
-        if(System.getProperty("os.name").toLowerCase().contains("windows"))
-            file = new File(currentProjectDirectory + "a.exe");
-        else if(System.getProperty("os.name").toLowerCase().contains("linux"))
-            file = new File(currentProjectDirectory + "a.out");
+        if(OS.contains("windows"))
+            file = new File(SUBMISSIONS_FOLDER + "a.exe");
+        else if(OS.contains("linux") | OS.contains("mac"))
+            file = new File(SUBMISSIONS_FOLDER + "a.out");
         file.delete();
 
         return count;
     }
 
-    public int evaluateJava(List<TestCases> testCases, File file, String currentProjectDirectory) throws Exception {
+    public int evaluateJava(List<TestCases> testCases, File file) throws Exception {
         int count = 0;
-        String cmd = "cmd /c javac " + file.getAbsolutePath();
-        Process process = Runtime.getRuntime().exec(cmd);
-        process.waitFor();
-        for (TestCases testCase : testCases) {
-            String input = testCase.getInput();
-            String output = testCase.getOutput();
+        if(OS.contains("windows")) {
+            String[] cmd = new String[] {"cmd" ,"/c" ,"javac " + file.getAbsolutePath()};
+            Process process = new ProcessBuilder(cmd).start();
+            process.waitFor();
+            for (TestCases testCase : testCases) {
+                String input = testCase.getInput();
+                String output = testCase.getOutput();
 
-            cmd = "cmd /c cd " + currentProjectDirectory;
-            cmd += " && java " + file.getName().split("\\.")[0] + " " + input;
-            count += compileAndRun(cmd, output);
+                cmd = new String[]{"cmd",
+                        "/c",
+                        "cd " + SUBMISSIONS_FOLDER + " && java " + file.getName().split("\\.")[0] + " " + input
+                };
+                count += compileAndRun(cmd, output);
+            }
+        }
+        else if(OS.contains("linux") | OS.contains("mac")){
+            String[] cmd = new String[] {"/bin/bash","-c","javac " + file.getAbsolutePath()};
+            Process process = new ProcessBuilder(cmd).start();
+            process.waitFor();
+            for(TestCases testCase : testCases) {
+                String input = testCase.getInput();
+                String output = testCase.getOutput();
+
+                cmd = new String[] {"/bin/bash",
+                        "-c",
+                        "cd " + SUBMISSIONS_FOLDER + " && java " + file.getName().split("\\.")[0] + " " + input
+                };
+                count += compileAndRun(cmd, output);
+            }
         }
 
         file.delete();
-        file = new File(currentProjectDirectory + file.getName().split("\\.")[0] + ".class");
+        file = new File(SUBMISSIONS_FOLDER + file.getName().split("\\.")[0] + ".class");
         file.delete();
         return count;
     }
 
-    public int evaluatePython(List<TestCases> testCases, File file, String currentProjectDirectory, int pythonVersion) throws Exception {
+    public int evaluatePython(List<TestCases> testCases, File file, int pythonVersion) throws Exception {
         int count = 0;
-        String cmd = "";
+        String[] cmd;
         for (TestCases testCase : testCases) {
             String input = testCase.getInput();
             String output = testCase.getOutput();
-            cmd = "cmd /c cd " + currentProjectDirectory;
-            if(pythonVersion == 2)
-                cmd += " && python2 " + file.getName() + " " + input;
-            else if(pythonVersion == 3)
-                cmd += " && python " + file.getName() + " " + input;
-            count += compileAndRun(cmd, output);
+            if(OS.contains("windows")) {
+                if (pythonVersion == 2)
+                    cmd = new String[]{"cmd",
+                            "/c",
+                            "cd " + SUBMISSIONS_FOLDER + " && python2 " + file.getName() + " " + input
+                };
+                else
+                    cmd = new String[]{"cmd",
+                            "/c",
+                            "cd " + SUBMISSIONS_FOLDER + " && python " + file.getName() + " " + input
+                    };
+                count += compileAndRun(cmd, output);
+            }
+            else if(OS.contains("linux") | OS.contains("mac")){
+                if (pythonVersion == 2)
+                    cmd = new String[]{"/bin/bash",
+                            "-c",
+                            "cd " + SUBMISSIONS_FOLDER + " && python2 " + file.getName() + " " + input
+                    };
+                else
+                    cmd = new String[]{"/bin/bash",
+                            "-c",
+                            "cd " + SUBMISSIONS_FOLDER + " && python " + file.getName() + " " + input
+                    };
+                count += compileAndRun(cmd, output);
+            }
         }
         file.delete();
         return count;
-    }
-
-    public String getCurrentProjectDirectory() throws Exception {
-        return System.getProperty("user.dir");
     }
 
     public List<Solutions> getAllSolutions() {
@@ -265,53 +343,4 @@ public class SolutionsServices {
         solutionsRepositories.deleteSolutionsByProblemId(problemId);
     }
 
-    /**
-     * We might not need this, instead we can use the addSolution itself.
-     */
-    /*public ResponseEntity updateSolution(Long userId, Long problemId, Long solutionId, MultipartFile file) throws Exception {
-        Optional<Users> user = usersRepositories.findById(userId);
-        if (user.isPresent()) {
-            Optional<Problems> problem = problemsRepositories.findById(problemId);
-            if (problem.isPresent()) {
-                Optional<Solutions> previousSolution = solutionsRepositories.findById(solutionId);
-                if (previousSolution.isPresent()) {
-                    long previousScore = previousSolution.get().getScore();
-                    user.get().setScore((user.get().getScore() - previousScore));
-
-                    List<TestCases> testCases = problem.get().getTestCases();
-                    if (testCases == null)
-                        return ResponseEntity.noContent().build();
-
-                    String currentProjectDirectory = getCurrentProjectDirectory();
-                    currentProjectDirectory += "\\src\\main\\resources\\Submissions\\";
-                    File userSolution = new File(currentProjectDirectory + file.getOriginalFilename());
-                    file.transferTo(userSolution);
-                    int totalTestCases = problem.get().getNumOfTestCases();
-                    int testCasesPassed = evaluateJava(testCases, userSolution, currentProjectDirectory);
-                    int testCasesFailed = totalTestCases - testCasesPassed;
-                    long score = (long) (((problem.get().getScore()) / totalTestCases) * testCasesPassed);
-                    user.get().setScore(user.get().getScore() + score);
-                    Solutions solution = new Solutions(
-                            file.getBytes(),
-                            testCasesPassed,
-                            testCasesFailed,
-                            score,
-                            problem.get(),
-                            user.get()
-                    );
-                    solution.setId(solutionId);
-                    usersRepositories.save(user.get());
-                    solutionsRepositories.save(solution);
-
-                    return ResponseEntity.accepted().build();
-                } else {
-                    return ResponseEntity.notFound().build();
-                }
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }*/
 }
